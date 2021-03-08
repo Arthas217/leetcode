716dea104a6c4e13979de95ffbf298b37ef04927
zuocheng <442854399@qq.com>
2021-03-08 18:47:56 +0800

    array

--- src/main/java/leetcode/ArrayTopic.java
+++ src/main/java/leetcode/ArrayTopic.java
@@ -7,6 +7,59 @@ package leetcode;
  */
 public class ArrayTopic {
 
+
+    /**
+     * 35. 搜索插入位置（目标值不存在数组中，返回它将会被按顺序插入的位置）
+     */
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length - 1;
+        int mid;
+        while (l < r) {
+            mid = (l + r) >> 1;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        //l == r
+        return nums[l] < target ? l + 1 : l;
+    }
+
+    public int searchInsert2(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public int searchInsert3(int[] arr, int left, int right) {
+        int mid;
+        while (left <= right) {
+            mid = (left + right) / 2;
+            // 如果中间元素的值和下标不相等，并且它前面一个元素和它的下标不相等，只需要查找左半边
+            if (arr[mid] == mid + 2) {
+                right = mid - 1;
+            } else if (arr[mid] == mid + 1) {
+                // 如果中间元素的值和下标不相等，并且它前面一个元素和它的下标相等，中间的数字下标就是不存在的数字插入的位置
+                left = mid + 1;
+            }
+        }
+        // 如果中间元素的值和下标相等，只需要查找右半边；
+        return left + 1;
+    }
+
     SortTopic sortTopic = new SortTopic();
 
     /**
