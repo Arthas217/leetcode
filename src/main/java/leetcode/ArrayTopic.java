72a5f31e39dd694cf0d6e94e6f463da5f8dfef39
zuocheng <442854399@qq.com>
2021-03-08 20:59:09 +0800

    array

--- src/main/java/leetcode/ArrayTopic.java
+++ src/main/java/leetcode/ArrayTopic.java
@@ -60,6 +60,21 @@ public class ArrayTopic {
         return left + 1;
     }
 
+    public int searchInsert4(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            // 分别处理如下三种情况
+            // 目标值在数组所有元素之前
+            // 目标值等于数组中某一个元素
+            // 目标值插入数组中的位置
+            if (nums[i] >= target) {
+                // 一旦发现大于或者等于target的num[i]，那么i就是我们要的结果
+                return i;
+            }
+        }
+        // 目标值在数组所有元素之后的情况
+        return nums.length; // 如果target是最大的，或者nums为空，则返回nums的长度
+    }
+
     SortTopic sortTopic = new SortTopic();
 
     /**
